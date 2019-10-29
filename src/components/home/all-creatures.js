7d9df2fd33e2717468e20302e021d7ea9fa51451
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-10-29 12:27:43 +0100

    feat: output list of all creatures on index

---
+++ src/components/home/all-creatures.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const tidyCreatureData = edge => {
+  return {
+    name: edge.node.frontmatter.title,
+    type: edge.node.frontmatter.type,
+    path: edge.node.frontmatter.path,
+  }
+}
+
+const AllCreatures = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      creatures: allMarkdownRemark(
+        filter: { frontmatter: { template: { eq: "creature" } } }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              type
+              template
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div>
+      {data.creatures.edges.map(edge => {
+        let creature = tidyCreatureData(edge)
+        return <div>{creature.name}</div>
+      })}
+    </div>
+  )
+
+  //   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+export default AllCreatures
