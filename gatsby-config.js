9633f997649338a1338da158bec88eba261207bd
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-11-07 16:01:35 +0100

    chore: add gh-pages to build

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`
   },
+  pathPrefix: `/wc5etools`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -35,7 +36,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
