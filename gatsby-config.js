d2b898e84538c0782057df493a770d4e045299a1
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-11-07 20:44:19 +0100

    feat: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Warcraft 5E Tools`,
+    description: `A collection of online tools developed for use with an unofficial third-party Warcraft supplement for Dungeons & Dragons 5th Edition.`,
+    author: `@orjanbp`
   },
   pathPrefix: `/wc5etools`,
   plugins: [
@@ -34,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
