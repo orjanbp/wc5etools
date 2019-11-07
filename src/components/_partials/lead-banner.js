edc7cbc1a6929ebfa903eaf71d08d384e5cec0e4
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-11-07 14:59:35 +0100

    feat: add image to lead-banner

--- src/components/_partials/lead-banner.js
+++ src/components/_partials/lead-banner.js
@@ -1,15 +1,21 @@
 import styled from "styled-components"
 import breakpoints from "./breakpoints"
 
+import BannerImageDark from "../../images/bannerimagedark.jpg"
+
 const LeadText = styled.div`
   & h1 {
     color: #c0392b;
+    filter: drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.6));
   }
 
   & p {
+    max-width: 600px;
     font-size: 2rem;
     font-weight: 300;
     color: #fafafa;
+    filter: drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.6))
+      drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.6));
   }
 
   @media ${breakpoints.xsmall} {
@@ -21,14 +27,21 @@ const LeadText = styled.div`
 
 const LeadButtons = styled.div`
   margin-top: 40px;
+
+  & a {
+    box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.3);
+  }
 `
 
 const LeadBanner = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
   background-color: #333;
+  background-image: url(${BannerImageDark});
+  background-size: cover;
   padding: 128px 0px;
   box-sizing: border-box;
 
