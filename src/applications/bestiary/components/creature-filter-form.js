f2d6c8fbad1a7f1be919093e555953e8abed926f
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-11-18 12:31:30 +0100

    refactor: move breakpoints, add flex-layout

--- src/applications/bestiary/components/creature-filter-form.js
+++ src/applications/bestiary/components/creature-filter-form.js
@@ -7,6 +7,7 @@ import _ from "lodash"
 import { BestiaryStore } from "../stores/bestiary-store"
 import { getCreatureTypes } from "../data/creature-types"
 
+import { FlexRow, FlexCol } from "../../../components/flex-layout"
 import CreatureFilterCheckbox from "./creature-filter-checkbox"
 
 const CreatureFilterContainer = styled.div`
@@ -17,21 +18,23 @@ const CreatureTypeSelection = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  margin-left: -4px;
-  margin-right: -4px;
+  justify-content: space-between;
+  width: 100%;
 
   & > * {
-    width: calc((100% / 6) - 8px);
+    width: calc((100% / 6) - 4px);
+    margin-bottom: 4px;
   }
 `
 
 const NameSearch = styled.input`
   height: 48px;
-  width: calc(50% - 4px);
+  width: 100%;
   background-color: #fff;
   border: 1px solid #d1d1d1;
   border-radius: 4px;
   margin-top: 16px;
+  margin-bottom: 0px;
 `
 
 const CreatureFilterForm = () => {
@@ -57,24 +60,33 @@ const CreatureFilterForm = () => {
 
   return (
     <CreatureFilterContainer>
-      <CreatureTypeSelection>
-        {creatureTypes.map((type) => {
-          return (
-            <CreatureFilterCheckbox
-              key={`creature-type-${type}`}
-              label={type}
-              onChange={(e) => {
-                filterTypeAction(type, e.target.checked)
-              }}
-            />
-          )
-        })}
-      </CreatureTypeSelection>
-      <NameSearch
-        type='text'
-        placeholder='Search Creature Names'
-        onChange={(e) => filterNameAction(e.target.value)}
-      />
+      <FlexRow>
+        <FlexCol as={CreatureTypeSelection} md={12}>
+          {creatureTypes.map((type) => {
+            return (
+              <CreatureFilterCheckbox
+                key={`creature-type-${type}`}
+                label={type}
+                onChange={(e) => {
+                  filterTypeAction(type, e.target.checked)
+                }}
+              />
+            )
+          })}
+        </FlexCol>
+      </FlexRow>
+      <FlexRow>
+        <FlexCol md={9}>
+          <NameSearch
+            type='text'
+            placeholder='Search Creature Names'
+            onChange={(e) => filterNameAction(e.target.value)}
+          />
+        </FlexCol>
+        <FlexCol md={3}>
+          <button>Reset Filters</button>
+        </FlexCol>
+      </FlexRow>
     </CreatureFilterContainer>
   )
 }
