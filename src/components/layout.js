5fa5aab42eacb831fa72e5123f3f2f19b4388e84
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-11-18 12:36:48 +0100

    refactor: '_partials' to 'partials'

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import styled, { css } from "styled-components"
 
-import Header from "./_partials/header"
-import Footer from "./_partials/footer"
+import Header from "./partials/header"
+import Footer from "./partials/footer"
 import backgroundLight from "../images/background-light.png"
 import breakpoints from "./breakpoints"
 
