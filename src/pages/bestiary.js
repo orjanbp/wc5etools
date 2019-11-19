029c7477cdb9835b1514d3d983d7e59833c18528
Ørjan Blytt Polden <oerjanbpolden@gmail.com>
2019-11-19 11:14:30 +0100

    refactor: move bestiary page layout into app.js

--- src/pages/bestiary.js
+++ src/pages/bestiary.js
@@ -1,16 +1,13 @@
 import React from "react"
-import { Layout, LayoutInner } from "../components/layout"
 import SEO from "../components/seo"
 
 import BestiaryApp from "../applications/bestiary/app"
 
 const BestiaryPage = () => (
-  <Layout>
+  <>
     <SEO title='Bestiary' />
-    <LayoutInner>
-      <BestiaryApp />
-    </LayoutInner>
-  </Layout>
+    <BestiaryApp />
+  </>
 )
 
 export default BestiaryPage
